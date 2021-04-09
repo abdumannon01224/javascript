2934adb8d7688bde1c11eed57865d881dcdbd515
Abdumannon <2002maxkamov@gmail.com>
2021-04-09 11:32:59 +0500

    javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+var myName = prompt ('Ismingizni kiriting')
+
+console.log(myName);
+ 
+
+
+var myYears = prompt ('Yoshingizni kiriting')
+
+console.log(myYears);
+
+var son = prompt ('Misolni yeching 24 + 24 =?')
+
+console.log('misol 1: 24 + 24 = 48 sizni jovobingiz'), console.log(son);
+
+var son = prompt ('Misolni yeching 24 - 24 =?')
+
+console.log('misol 1: 24 - 24 = 0 sizni jovobingiz'), console.log(son);
+
+var son = prompt ('Misolni yeching 20 % 5 =?')
+
+console.log('misol 1: 20 % 5 = 0 sizni jovobingiz'), console.log(son);
+
+var son = prompt ('Misolni yeching 12/3 =?')
+
+console.log('misol 1: 12/3 = 4 sizni jovobingiz'), console.log(son);
+
+var a = alert ('jovoblarni korish uchun console kiring')
